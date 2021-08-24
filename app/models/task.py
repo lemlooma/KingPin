9b74f3d933553a4b0ffaf1e35e5d7bd0d3d34533
Lama El-Sherbiny <lamasherb@gmail.com>
2021-08-24 17:09:14 -0400

    models

---
+++ app/models/task.py
@@ -0,0 +1,27 @@
+from .db import db
+
+
+class Task(db.Model):
+    __tablename__ = 'tasks'
+
+    id = db.Column(db.Integer, primary_key=True)
+    project_id = db.Column(db.Integer, db.ForeignKey("projects.id"))
+    complete = db.Column(db.Boolean, default=False)
+    title = db.Column(db.String(255))
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=True)
+    due_date = db.Column(db.Date, nullable=True)
+    description = db.Column(db.Text, nullable=True)
+
+    project = db.relationship("Project", back_populates="tasks")
+    user = db.relationship("User", back_populates="tasks")
+    
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "project_id": self.project_id,
+            "complete": self.complete,
+            "title": self.title,
+            "user_id": self.user_id,
+            "due_date": self.due_date,
+            "description": self.description
+        }
--- app/models/user.py
+++ app/models/user.py
@@ -1,6 +1,7 @@
 from .db import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from .user_team import user_team
 
 
 class User(db.Model, UserMixin):
@@ -10,6 +11,10 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(40), nullable=False, unique=True)
     email = db.Column(db.String(255), nullable=False, unique=True)
     hashed_password = db.Column(db.String(255), nullable=False)
+
+    projects = db.relationship("Project", back_populates="user")
+    tasks = db.relationship("Task", back_populates="user")
+    teams = db.relationship("Team", secondary=user_team, back_populates="users")
 
     @property
     def password(self):
@@ -24,7 +29,13 @@ class User(db.Model, UserMixin):
 
     def to_dict(self):
         return {
-            'id': self.id,
-            'username': self.username,
-            'email': self.email
+            "id": self.id,
+            "username": self.username,
+            "email": self.email
+        }
+
+    def to_dict_safe(self):
+        return {
+            "id": self.id,
+            "username": self.username,
         }
