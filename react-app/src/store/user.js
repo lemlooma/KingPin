718cf452fdb81582bb7b807e7ddf1cb49f8de3b1
Lama El-Sherbiny <lamasherb@gmail.com>
2021-08-25 19:09:17 -0400

    icon fix

---
+++ react-app/src/store/user.js
@@ -0,0 +1,26 @@
+const LOAD = "session/LOAD";
+
+const getUsers = (payload) => ({
+  type: LOAD,
+  payload,
+});
+
+export const getUsersFunction = () => async (dispatch) => {
+  const response = await fetch("/api/users/", {
+    headers: { "Content-Type": "application/json" },
+  });
+  if (response.ok) {
+    const users = await response.json();
+    dispatch(getUsers(users));
+  }
+};
+const initialState = { users: [] };
+
+export default function reducer(state = initialState, action) {
+  switch (action.type) {
+    case LOAD:
+      return { users: action.payload.users };
+    default:
+      return state;
+  }
+}
